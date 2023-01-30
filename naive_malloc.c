0d27a41d332f3fb4cf7e930d0c838e84a5dd29e4
jstevenson1337 <jmstevenson918@gmail.com>
2023-01-30 05:53:56 -0600

    corrected byte alignment

--- naive_malloc.c
+++ naive_malloc.c
@@ -13,10 +13,11 @@ void *naive_malloc(size_t size)
 {
 	void *ptr;
 
+	size = sizeof(ptr) * size;
 	if (size == 0)
 		printf("Size = %lu\n", size);
 		EXIT_FAILURE;
-	ptr = sbrk(size + 1);
+	ptr = sbrk(size);
 	printf("Starting Break is %lu\n", (size_t)ptr);
 	return (ptr);
 }

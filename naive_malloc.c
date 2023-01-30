74cd8041eb53a8d274b15a7db69c449c334828be
jstevenson1337 <jmstevenson918@gmail.com>
2023-01-30 04:59:12 -0600

    corrected prototy[e

--- naive_malloc.c
+++ naive_malloc.c
@@ -5,11 +5,13 @@
 /**
  * main - Program entry point
  *
+ * size: size passed in
  * Return: EXIT_SUCCESS or EXIT_FAILURE
  */
-int main(void)
+void *_malloc(size_t size);
 {
     void *ptr;
+    size = sizeof(ptr);
 
     ptr = sbrk(0);
     printf("Starting break is %p\n", ptr);

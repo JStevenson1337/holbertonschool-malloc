70104a8db5beef8c61c8dbe1c921fab6f05e6e59
Jeremy Stevenson <jstevenson918@gmail.com>
2023-02-01 22:30:16 -0600

    checking naive malloc

--- malloc.h
+++ malloc.h
@@ -1,58 +1,20 @@
-#ifndef MALLOC_H
-#define MALLOC_H
+#ifndef __MALLOC_H__
+#define __MALLOC_H__
 
-#include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/cdefs.h>
-#include <sys/types.h>
-#include <err.h>
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sysexits.h>
-#include <unistd.h>
-#include <assert.h>
 
-#ifndef PAGESIZE
-#define PAGESIZE 3 * sysconf(_SC_PAGESIZE); /* size of mem requsted during syscall to resieve from the kernal 12288 bytes*/
-#endif
+#define MALIGN(x) ((8 - 1 + x) & ~(8 - 1))
 
-#define BLOCK(ptr) ((void *)((unsigned long)ptr + sizeof(block_t)))
+#define MALLOCD_PAGE sysconf(_SC_PAGESIZE)
 
-/**
- *
- *
- *
- *
- */
- typedef struct Block
-{
-    struct Block *next;;
-    struct Block *prev;
-    size_t *size;
-} Block_t;
-
-typedef struct BITMAP
-{
-    size_t size;
-    uint8_t HEADBITS;
-    uint8_t TAILBITS;
-} BITMAP_t;
-
-#define Block_t -> *size = Bitmap_t -> &size;
-
-
-#define static Header root;
-#define static Header *toor = NULL;
-
-
+#define HEADER_S sizeof(size_t)
 
 void *naive_malloc(size_t size);
 void *_malloc(size_t size);
 void _free(void *ptr);
-void *_calloc(size_t nmemb, size_t size);
-void *_realloc(void *ptr, size_t size);
-#endif
+
+
+#endif /* __MALLOC_H__ */
--- naive_malloc.c
+++ naive_malloc.c
@@ -1,43 +1,28 @@
 #include "malloc.h"
-
 /**
-* naive_malloc -  basic malloc
-* @size: number of bytes required to allocate
-* Return: a pointer to memory allocated on success, NULL otherwise
+* naive_malloc - reimplementation of malloc
+* @size: bytes requested
+* Return: SUCCESS = pointer to allocated memory; FAIL = NULL
 */
 void *naive_malloc(size_t size)
 {
-    void *start, *baseptr = NULL, *heapptr = NULL, *ptr = NULL;
-    size_t page_size;
+	size_t chunk_hdr = MALIGN(size + HEADER_S);
+	static size_t block;
+	void *ptr;
+	static void *heap_ptr;
 
-
-
-    if (size == 0)
-        return (NULL);
-
-    if (!start)
-    {
-        heapptr = sbrk(page_size);
-        assert(heapptr != ((void *) -1) || heapptr != NULL);
-
-        start = heapptr;
-		memmove(heapptr, start, page_size);
-
-
-        printf("heap = %p\t start = %p\t pagesize = %lu",
-                   heapptr, start, (unsigned long int)page_size);
-
-    }
-	return (0);
-
-}
-
-
-
-int main()
-{
-    size_t var = 60;
-
-    naive_malloc(var);
-    return (0);
+	if (!size)
+		return (NULL);
+	while (block < chunk_hdr)
+	{
+		if (heap_ptr)
+			sbrk(0);
+		else
+			MALLOCD_PAGE, heap_ptr = sbrk(MALLOCD_PAGE);
+		block = block + MALLOCD_PAGE;
+	}
+	ptr = heap_ptr;
+	memcpy(ptr, &chunk_hdr, sizeof(chunk_hdr));
+	heap_ptr = (void *)((char *)(ptr) + chunk_hdr);
+	return ((void *)((char *)(ptr) + MALIGN(HEADER_S)));
 }
